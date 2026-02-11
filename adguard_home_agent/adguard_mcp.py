aec81670de4326852653bdfa6ebe82ef168530ea
KnucklesSG1 <knucklessg1@gmail.com>
2026-02-10 23:08:23 -0600

    Bump version: 0.2.3 → 0.2.4

--- adguard_home_agent/adguard_mcp.py
+++ adguard_home_agent/adguard_mcp.py
@@ -28,7 +28,7 @@ from adguard_home_agent.middlewares import (
     JWTClaimsLoggingMiddleware,
 )
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
