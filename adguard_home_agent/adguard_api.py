938fd08c7c991849d93ab4c3e52445dba6d8873d
KnucklesSG1 <knucklessg1@gmail.com>
2026-03-07 02:29:55 -0600

    Bump version: 0.2.30 → 0.2.31

--- adguard_home_agent/adguard_api.py
+++ adguard_home_agent/adguard_api.py
@@ -7,7 +7,7 @@ from typing import Dict, List, Optional, Any
 from urllib.parse import urljoin
 import urllib3
 
-__version__ = "0.2.30"
+__version__ = "0.2.31"
 
 
 class Api:
--- adguard_home_agent/agent.py
+++ adguard_home_agent/agent.py
@@ -12,7 +12,7 @@ from agent_utilities import (
     load_identity,
 )
 
-__version__ = "0.2.30"
+__version__ = "0.2.31"
 
 logging.basicConfig(
     level=logging.INFO,
