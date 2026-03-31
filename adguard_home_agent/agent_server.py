1995d3680478bd47e586b5b36124da105d2b05a9
KnucklesSG1 <knucklessg1@gmail.com>
2026-03-31 01:09:55 -0500

    Bump version: 0.2.50 → 0.2.51

--- adguard_home_agent/agent_server.py
+++ adguard_home_agent/agent_server.py
@@ -11,7 +11,7 @@ from agent_utilities import (
     load_identity,
 )
 
-__version__ = "0.2.50"
+__version__ = "0.2.51"
 
 
 logging.basicConfig(
--- adguard_home_agent/mcp_server.py
+++ adguard_home_agent/mcp_server.py
@@ -21,7 +21,7 @@ from agent_utilities.mcp_utilities import (
 )
 from adguard_home_agent.api_wrapper import Api
 
-__version__ = "0.2.50"
+__version__ = "0.2.51"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
