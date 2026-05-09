86fc2a5452f011a4ae56b09c7ef03545e6b6a0fd
knucklessg1 <knucklessg1@gmail.com>
2026-05-09 17:36:50 -0500

    Bump version: 0.9.0 → 0.10.0

--- adguard_home_agent/mcp_server.py
+++ adguard_home_agent/mcp_server.py
@@ -38,7 +38,7 @@ from starlette.responses import JSONResponse
 
 from adguard_home_agent.api_client import Api
 
-__version__ = "0.9.0"
+__version__ = "0.10.0"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
